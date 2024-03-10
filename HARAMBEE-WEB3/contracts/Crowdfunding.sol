62ccad4d2fdb37176c35a116eb3ed58779f897ed
cmm25 <craigmutegi@gmail.com>
2024-03-10 22:53:36 +0300

    donations/ contribution function added.

--- HARAMBEE-WEB3/contracts/Crowdfunding.sol
+++ HARAMBEE-WEB3/contracts/Crowdfunding.sol
@@ -41,7 +41,22 @@ contract CrowdFunding{
 
 
      }  
-    function contribute()   {}
+    function contribute(uint256 _id) public payable  {
+        uint256 amount = msg.value;
+        Campaign storage campaign  = campaigns[_id];
+
+        campaign.contributors.push(msg.sender);
+        campaign.contributions.push(amount);
+
+        // the transaction.
+        (bool sent, ) = payable(campaign.owner).call{value:amount}("");
+
+        if(sent){
+            campaign.raisedAmount = campaign.raisedAmount + amount;
+        }
+
+
+    }
     function getDonators()  {}
     function getCampaigns()  {}
     
