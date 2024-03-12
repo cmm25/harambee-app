6f26739e4755ab1a781ef5045db62b3bf0d8a840
cmm25 <craigmutegi@gmail.com>
2024-03-12 12:04:41 +0300

    removed vite and put create-react

---
+++ harambee2/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Route,Routes} from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className='relative sm:-8 p-4 bg-[#12120a] min-h screen flex flex-row'>App</div>
+  )
+}
+
+export default App
